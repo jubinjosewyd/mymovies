4d82e71d896c70ffae4b0faa20fcaf1de9d27304
Jubin Jose <jubinjosewyd@gmail.com>
2023-08-14 11:26:05 +0530

    commit my movies

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  trendingMovies: any;
+  theatreMovies: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getTrendingMovies();
+    this.getThreatreMovies();
+  }
+
+  getTrendingMovies() {
+    this.http.get('http://localhost:4200/assets/data/trending-movies.json').subscribe((movies) => {
+      this.trendingMovies = movies;
+      console.log(this.trendingMovies)
+    });
+}
+getThreatreMovies() {
+  this.http.get('http://localhost:4200/assets/data/theatre-movies.json').subscribe((movies) => {
+    this.theatreMovies = movies;
+    console.log(this.theatreMovies)
+  });
+}
+}
